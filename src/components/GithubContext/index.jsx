31dbe98df1153f3d91cc2857f93e4b51ddad21c0
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-06-21 13:22:50 +0200

    UPDATE: Added PropTypes and ability to change endpoint via hooks

--- src/components/GithubContext/index.jsx
+++ src/components/GithubContext/index.jsx
@@ -1,36 +1,45 @@
 import React from 'react'
 import Axios from 'axios';
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 export const GithubContext = React.createContext({
-    apiPath: null,
     data: null,
+    endpoint: null,
     setData: () => null,
+    setEndpoint: () => null,
 })
 
 export const GithubContextConsumer = GithubContext.Consumer;
 
-export const GithubContextProvider = ({ children, endpoint }) => {
-    const [data, setData] = React.useState(null);
+export const GithubContextProvider = ({ children, root, rootFile }) => {
+    const [endpoint, setEndpoint] = React.useState(`${root}${rootFile}`)
+
+    const [data, setData] = React.useState(null)
     React.useEffect(() => {
+        console.log(`${root}${rootFile}`)
         if (endpoint) {
             Axios
                 .get(endpoint)
                 .then((res) => setData(res.data))
+                .catch((err) => console.log(err))
         }
     }, [endpoint])
 
     return (
-        <GithubContext.Provider value={{ data }} >
+        <GithubContext.Provider value={{ data, root, endpoint, setEndpoint }} >
             {children}
         </GithubContext.Provider>
     )
 }
 
-// GithubContextProvider.propTypes = {
-//     children: PropTypes.object,
-//   };
-  
-  GithubContextProvider.defaultProps = {
-    children: {},
+GithubContextProvider.propTypes = {
+    children: PropTypes.object,
+    root: PropTypes.string.isRequired,
+    rootFile: PropTypes.string.isRequired,
   };
+
+GithubContextProvider.defaultProps = {
+    children: {},
+    root: null,
+    rootFile: null,
+};
