a5a99c9abb651e2f090678353b12505a8706c266
Jens Kristian Hoel <github@hoelweb.com>
2019-06-21 23:21:25 +0200

    FIX: corrected root uri for RepoMenu component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import MarkdownBrowser from './components/MarkdownBrowser'
-import { GithubContextProvider } from './components/GithubContext'
-import RepoMenu from './components/RepoMenu'
+import MarkdownBrowser from './components/MarkdownBrowser';
+import { GithubContextProvider } from './components/GithubContext';
+import RepoMenu from './components/RepoMenu';
 
 function App() {
   return (
-    <GithubContextProvider root="https://raw.githubusercontent.com/jkhoel/github-readme-viewer/master" rootFile="/README.md">
-      <RepoMenu root="https://raw.githubusercontent.com/jkhoel/github-readme-viewer/master" />
+    <GithubContextProvider
+      root="https://raw.githubusercontent.com/jkhoel/github-readme-viewer/master"
+      rootFile="/README.md"
+    >
+      <RepoMenu root="https://api.github.com/repos/jkhoel/github-readme-viewer/contents/" />
       <MarkdownBrowser />
     </GithubContextProvider>
   );
 }
 
 export default App;
-
-
