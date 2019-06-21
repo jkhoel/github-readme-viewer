d3e308aade2450459d822d0b3d7a6dcb99b07878
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-06-21 13:22:15 +0200

    CHANGE: Uses new MarkdownBrowser component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import GitReadme from './components/GitReadme'
+import MarkdownBrowser from './components/MarkdownBrowser'
 import { GithubContextProvider } from './components/GithubContext'
 
 function App() {
   return (
-    <GithubContextProvider endpoint="https://api.github.com/repos/132nd-vWing/OPUF-Brief/readme">
-      <GitReadme />
+    <GithubContextProvider root="https://raw.githubusercontent.com/132nd-vWing/OPUF-Brief/readme-patch-1" rootFile="/README.md">
+      <MarkdownBrowser />
     </GithubContextProvider>
   );
 }
