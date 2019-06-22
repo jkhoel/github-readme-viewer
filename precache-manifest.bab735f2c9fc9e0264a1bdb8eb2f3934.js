10957e26aa311444fa108c01ea6df5219ca62745
Jens Kristian Hoel <github@hoelweb.com>
2019-06-22 20:29:51 +0200

    Updates

---
+++ precache-manifest.bab735f2c9fc9e0264a1bdb8eb2f3934.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1dc8c2b68b1059c31d1abda7db1b1e",
+    "url": "/github-readme-viewer/index.html"
+  },
+  {
+    "revision": "b84456e95a2cf9904d7f",
+    "url": "/github-readme-viewer/static/css/main.bdc9d1ad.chunk.css"
+  },
+  {
+    "revision": "f045cf1620c1e9529476",
+    "url": "/github-readme-viewer/static/js/2.96f97de9.chunk.js"
+  },
+  {
+    "revision": "b84456e95a2cf9904d7f",
+    "url": "/github-readme-viewer/static/js/main.13d6adca.chunk.js"
+  },
+  {
+    "revision": "ccc22eca363c9617d894",
+    "url": "/github-readme-viewer/static/js/runtime~main.4a6c9c21.js"
+  }
+]);
