11e7367bbf95a5c519ea2a0df4b1167093ac54f8
Jens Kristian Hoel <github@hoelweb.com>
2019-06-22 02:33:58 +0200

    FIX: propTypes.children now PropTypes.array

--- src/components/GithubContext/index.jsx
+++ src/components/GithubContext/index.jsx
@@ -1,44 +1,43 @@
-import React from 'react'
+import React from 'react';
 import Axios from 'axios';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 export const GithubContext = React.createContext({
-    data: null,
-    endpoint: null,
-    setData: () => null,
-    setEndpoint: () => null,
-})
+  data: null,
+  endpoint: null,
+  setData: () => null,
+  setEndpoint: () => null
+});
 
 export const GithubContextConsumer = GithubContext.Consumer;
 
 export const GithubContextProvider = ({ children, root, rootFile }) => {
-    const [endpoint, setEndpoint] = React.useState(`${root}${rootFile}`)
+  const [endpoint, setEndpoint] = React.useState(`${root}${rootFile}`);
 
-    const [data, setData] = React.useState(null)
-    React.useEffect(() => {
-        if (endpoint) {
-            Axios
-                .get(endpoint)
-                .then((res) => setData(res.data))
-                .catch((err) => console.log(err))
-        }
-    }, [endpoint])
+  const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    if (endpoint) {
+      Axios.get(endpoint)
+        .then(res => setData(res.data))
+        .catch(err => console.log(err));
+    }
+  }, [endpoint]);
 
-    return (
-        <GithubContext.Provider value={{ data, root, endpoint, setEndpoint }} >
-            {children}
-        </GithubContext.Provider>
-    )
-}
+  return (
+    <GithubContext.Provider value={{ data, root, endpoint, setEndpoint }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
 
 GithubContextProvider.propTypes = {
-    children: PropTypes.object,
-    root: PropTypes.string.isRequired,
-    rootFile: PropTypes.string.isRequired,
-  };
+  children: PropTypes.array,
+  root: PropTypes.string.isRequired,
+  rootFile: PropTypes.string.isRequired
+};
 
 GithubContextProvider.defaultProps = {
-    children: {},
-    root: null,
-    rootFile: null,
+  children: {},
+  root: null,
+  rootFile: null
 };
