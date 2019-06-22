13ace50f846f98511880458a49f2f909dcdf2e91
Jens Kristian Hoel <github@hoelweb.com>
2019-06-22 22:11:53 +0200

    CHANGE: Added ImageModal component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,36 @@ import React from 'react';
 import { Grid, GridItem } from 'styled-grid-component';
 import MarkdownBrowser from './components/MarkdownBrowser';
 import { GithubContextProvider } from './components/GithubContext';
+import { ImageModalContextProvider } from './components/ImageModalContext';
 import RepoMenu from './components/RepoMenu';
+import ImageModal from './components/ImageModalContext/ImageModal';
 
 function App() {
   return (
-    <Grid
-      width="100%"
-      height="100vh"
-      templateColumns="repeat(5, 1fr)"
-      gap="10px"
-      autoRows="minmax(100px, auto)"
-    >
-      <GithubContextProvider
-        root="https://raw.githubusercontent.com/132nd-vWing/OPUF-Brief/master"
-        rootFile="/README.md"
+    <ImageModalContextProvider>
+      <Grid
+        width="100%"
+        height="100vh"
+        templateColumns="repeat(5, 1fr)"
+        gap="10px"
+        autoRows="minmax(100px, auto)"
       >
-        <GridItem column="1" row="1">
-          <RepoMenu root="https://api.github.com/repos/132nd-vWing/OPUF-Brief/contents/" />
-        </GridItem>
-        <GridItem column="2 / 6" row="1">
-          <MarkdownBrowser />
-        </GridItem>
-      </GithubContextProvider>
-    </Grid>
+        <GithubContextProvider
+          root="https://raw.githubusercontent.com/132nd-vWing/OPUF-Brief/master"
+          rootFile="/README.md"
+        >
+          <GridItem column="1" row="1">
+            <RepoMenu root="https://api.github.com/repos/132nd-vWing/OPUF-Brief/contents/" />
+          </GridItem>
+          <GridItem column="2 / 6" row="1">
+            <MarkdownBrowser />
+          </GridItem>
+          <GridItem column="2 / 6" row="1">
+            <ImageModal />
+          </GridItem>
+        </GithubContextProvider>
+      </Grid>
+    </ImageModalContextProvider>
   );
 }
 
