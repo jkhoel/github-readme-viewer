0277ccda1c083dd2a93aaf2983b1b91cc3adc0fc
Jens Kristian Hoel <github@hoelweb.com>
2019-06-22 13:30:35 +0200

    CHANGE: components are now arranged on a css grid

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Grid, GridItem } from 'styled-grid-component';
 import MarkdownBrowser from './components/MarkdownBrowser';
 import { GithubContextProvider } from './components/GithubContext';
 import RepoMenu from './components/RepoMenu';
 
 function App() {
   return (
-    <GithubContextProvider
-      root="https://raw.githubusercontent.com/jkhoel/github-readme-viewer/master"
-      rootFile="/README.md"
+    <Grid
+      width="100%"
+      height="100vh"
+      templateColumns="repeat(5, 1fr)"
+      gap="10px"
+      autoRows="minmax(100px, auto)"
     >
-      <RepoMenu root="https://api.github.com/repos/jkhoel/github-readme-viewer/contents/" />
-      <MarkdownBrowser />
-    </GithubContextProvider>
+      <GithubContextProvider
+        root="https://raw.githubusercontent.com/jkhoel/github-readme-viewer/master"
+        rootFile="/README.md"
+      >
+        <GridItem column="1" row="1">
+          <RepoMenu root="https://api.github.com/repos/jkhoel/github-readme-viewer/contents/" />
+        </GridItem>
+        <GridItem column="2 / 6" row="1">
+          <MarkdownBrowser />
+        </GridItem>
+      </GithubContextProvider>
+    </Grid>
   );
 }
 
